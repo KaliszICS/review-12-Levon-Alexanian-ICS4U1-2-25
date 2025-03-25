dd1e558cf3a21d7a0ce42116e51c9d8392b8f032
Levon-Alexanian-ICS4U1-2-25 <levon.alexanian@student.tdsb.on.ca>
2025-03-25 04:46:47 +0000

    update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,6 +1,36 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static boolean isPalindrome(String word) {
+		word = word.replaceAll(" ", "").toLowerCase();
+		for (int i = 0; i < word.length(); i++) {
+			if (word.charAt(i) == word.charAt(word.length()-1-i)) {
+				continue;
+			} else {
+				return false;
+			}
+		}
+		return true;
+	}
 
+	public static int getAge(String[] names, int[] ages, String person) {
+		for (int i = 0; i < names.length; i++) {
+            if (names[i].equalsIgnoreCase(person)) {  
+                int age = ages[i];
+				return age;
+            }
+
+		}
+		return -1;
+	}
+
+	public static int countWords(String words, char letter) {
+		int counter = 0;
+		String[] words2 = words.split(" ");
+		for (String i : words2) {
+			if (i.indexOf(letter) >= 0) {
+				counter++;
+		}
+	}
+		return counter;
 	}
 }
